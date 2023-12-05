f3eae3f239117185243640e6a8bdfdd40cefc88c
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-05 23:01:33 +0700

    menambahkan class EntryViewModel

--- app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
@@ -1,11 +1,46 @@
 package com.example.roomsiswaa.model
-import com.example.roomsiswaa.data.Siswa
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswaa.data.Siswa
+import com.example.roomsiswaa.repositori.RepositoriSiswa
+
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    /*
+       *Berisi status Siswa saat ini
+        */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
+
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
