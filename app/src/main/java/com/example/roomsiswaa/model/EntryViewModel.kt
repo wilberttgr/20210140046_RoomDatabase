0767dd0d7db4adc24028a9c86c469d1327df251a
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-05 22:58:38 +0700

    add data class UIStateSiswa

--- app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
@@ -1,5 +1,10 @@
 package com.example.roomsiswaa.model
 
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
