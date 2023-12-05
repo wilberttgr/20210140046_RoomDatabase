61c374b8b96066540c34e4aa428a49316ba921a9
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-06 00:36:37 +0700

    menambahkan fun ListSiswa

--- app/src/main/java/com/example/roomsiswaa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswaa/ui/theme/halaman/HalamanHome.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
@@ -20,6 +22,22 @@ import androidx.compose.ui.unit.dp
 import com.example.roomsiswaa.R
 import com.example.roomsiswaa.data.Siswa
 
+
+@Composable
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = Modifier) {
+        items(items = itemSiswa, key = { it.id }) { person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
 @Composable
 fun DataSiswa(
     siswa: Siswa,
