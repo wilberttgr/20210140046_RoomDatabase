29bb04a1ef7e2c25aaea46c69038a960e988b899
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-05 22:59:48 +0700

    add fun DetailSiswa

--- app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
@@ -1,4 +1,5 @@
 package com.example.roomsiswaa.model
+import com.example.roomsiswaa.data.Siswa
 
 
 data class UIStateSiswa(
@@ -10,4 +11,12 @@ data class DetailSiswa(
     val nama: String = "",
     val alamat: String = "",
     val telpon: String = "",
+)
+
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya */
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
