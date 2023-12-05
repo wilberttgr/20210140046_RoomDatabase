8af0476e42ed3a6bcbd750c58b6595e54f9fac45
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-05 23:05:49 +0700

    menambahkan fun Siswa to UIStateSiswa

--- app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswaa/model/EntryViewModel.kt
@@ -55,6 +55,11 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     alamat = alamat,
     telpon = telpon
 )
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
     nama = nama,
