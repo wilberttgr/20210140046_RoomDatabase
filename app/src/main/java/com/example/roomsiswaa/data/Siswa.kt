5ee03aa788751dd49e5d2b565d265ce479f29907
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-05 20:00:57 +0700

    add class DatabaseSiswa, siswa, and interface SiswaDao

---
+++ app/src/main/java/com/example/roomsiswaa/data/Siswa.kt
@@ -0,0 +1,13 @@
+package com.example.roomsiswaa.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
---
+++ app/src/main/java/com/example/roomsiswaa/data/SiswaDao.kt
@@ -0,0 +1,27 @@
+package com.example.roomsiswaa.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+}
