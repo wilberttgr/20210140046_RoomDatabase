d3ab893a4b87b8fb503fe3a0309ac34ff91ecea0
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-06 00:37:29 +0700

    menambahkan fun BodyHome

--- app/src/main/java/com/example/roomsiswaa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswaa/ui/theme/halaman/HalamanHome.kt
@@ -16,13 +16,38 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.roomsiswaa.R
 import com.example.roomsiswaa.data.Siswa
 
-
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemSiswa.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.deskripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+            ListSiswa(
+                itemSiswa = itemSiswa,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
 @Composable
 fun ListSiswa(
     itemSiswa: List<Siswa>,
