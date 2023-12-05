dfe5060c3187310280e90c8f17e8d3ef07280026
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-05 23:35:52 +0700

    Menambahkan Fungsi ekstensi query

--- app/src/main/java/com/example/roomsiswaa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswaa/model/PenyediaViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.roomsiswaa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswaa.AplikasiSiswa
+
 object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+    }
+    /**
+     * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari [AplikasiSiswa].
+     */
+    fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 }
