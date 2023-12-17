9d1ebe2d964b90dd8b02eafbff4700c2baddbad4
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-17 15:22:02 +0700

    Done

--- app/src/main/java/com/example/roomsiswaa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswaa/navigasi/PengelolaHalaman.kt
@@ -29,10 +29,11 @@ import com.example.roomsiswaa.ui.theme.halaman.ItemEditScreen
 
 
 @Composable
-fun SiswaApp(navController: NavHostController= rememberNavController()) {
+fun SiswaApp(navController: NavHostController = rememberNavController()) {
     HostNavigasi(navController = navController)
 
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
@@ -58,45 +59,54 @@ fun SiswaTopAppBar(
         }
     )
 }
+
 @Composable
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-){
-    NavHost(navController=navController, startDestination = DestinasiHome.route,modifier = Modifier)
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    )
     {
-        composable(DestinasiHome.route){
+        composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 onDetailClick = {
                     navController.navigate("${DetailsDestination.route}/$it")
-                }
+                },
             )
         }
-        composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() },
+            )
         }
 
         composable(
             DetailsDestination.routeWithArgs,
-            arguments = listOf(navArgument(DetailsDestination.siswaIdArg){
+            arguments = listOf(navArgument(DetailsDestination.siswaIdArg) {
                 type = NavType.IntType
             })
-        ){
+        ) {
             DetailsScreen(
-                navigateBack = {navController.popBackStack()},
+                navigateBack = { navController.popBackStack() },
                 navigateToEditItem = {
-                    navController.navigate("$(ItemEditDestination.route}/$it")
+                    navController.navigate("${ItemEditDestination.route}/$it")
                 }
             )
         }
         composable(
             ItemEditDestination.routeWithArgs,
-            arguments = listOf(navArgument(ItemEditDestination.itemIdArg){
+            arguments = listOf(navArgument(ItemEditDestination.itemIdArg) {
                 type = NavType.IntType
             })
-        ){
-            ItemEditScreen(navigateBack = { navController.popBackStack() }, onNavigateUp = { navController.navigateUp() })
+        ) {
+            ItemEditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() })
         }
     }
 }
